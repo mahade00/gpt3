1b0791a860b9dfb795b5e9e9e06ae483a7182396
mahade00 <mahade.hassan.bd@gmail.com>
2023-08-08 12:55:11 +0600

    header text

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import './header.css'
+import './header.css';
 const Header = () => {
     return (
-        <div>
-            header
+        <div className='gpt3__header section__padding' id='home'>
+            <div className='gpt3__header-content'>
+                <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+                <div className='gpt3__header-content__input'>
+                    <input type='text' placeholder=' Your Email Address'/>
+                </div>
+            </div>
         </div>
     );
 };
