96a2cd19e0a32fca37621353e8972b26033a8280
mahade00 <mahade.hassan.bd@gmail.com>
2023-08-10 07:28:13 +0600

    blog responsive

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -10,7 +10,7 @@ const Article = ({imgUrl, date, title}) => {
             <div className='gpt3__blog-container_article-content'>
                 <div>
                     <p>{date}</p>
-                    <h4>{ title}</h4>
+                    <h3>{ title}</h3>
                 </div>
                 <p>Read Full Article</p>
             </div>
